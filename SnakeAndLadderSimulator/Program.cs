107ac4455fbc01da3ab0c7a5497c052bc22168fd
Anant1202 <anantsingh1202@gmail.com>
2022-05-06 16:54:14 +0530

    Added:Start Position of Player 1

--- SnakeAndLadderSimulator/Program.cs
+++ SnakeAndLadderSimulator/Program.cs
@@ -7,6 +7,8 @@ namespace SnakeAndLadderSimulator
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake & Ladder Simulator Program");
+            Snake_Ladder_Simulator ob1= new Snake_Ladder_Simulator();
+            ob1.DieRoll();
         }
     }
 }
