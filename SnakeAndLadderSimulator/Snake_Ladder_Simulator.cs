827abded4a89ebeb0c9eb9adf4bed8a03521591d
Anant1202 <anantsingh1202@gmail.com>
2022-05-06 17:07:22 +0530

    Added:Die rolled

--- SnakeAndLadderSimulator/Snake_Ladder_Simulator.cs
+++ SnakeAndLadderSimulator/Snake_Ladder_Simulator.cs
@@ -9,10 +9,11 @@ namespace SnakeAndLadderSimulator
     public class Snake_Ladder_Simulator
     {
         int Start_Position=0;
-        
+        Random getnum = new Random();
         public void DieRoll()
         {
-
+            int num = getnum.Next(1, 7);
+            Console.WriteLine(num);
         }
     }
 }
